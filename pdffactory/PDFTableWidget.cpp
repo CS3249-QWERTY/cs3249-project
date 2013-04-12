4379cc49d6ddb4f4e78a58f74adbedab634e48d7
Bui Phuc Duyet <zuyetawarmatik@ubuntu.(none)>
2013-04-12 20:36:17 +0700

    Multiple selected with Ctrl+Left Mouse

--- pdffactory/PDFTableWidget.cpp
+++ pdffactory/PDFTableWidget.cpp
@@ -62,19 +62,30 @@ void PDFTableWidget::registerPage(PDFPageWidget* child){
 void PDFTableWidget::pageClicked(PDFPageWidget *sender, QMouseEvent* event, QString path){
     if (event->button() == Qt::LeftButton){
         // Handle selection
-        for (int i = 0; i < selectedPages.size(); i++) {
-            selectedPages.at(i)->setSelected(false);
+
+        if (event->modifiers() != Qt::ControlModifier) {
+            for (int i = 0; i < selectedPages.size(); i++) {
+                selectedPages.at(i)->setSelected(false);
+            }
+
+            selectedPages.clear();
+
+            if (!sender->isSelected()) {
+                sender->setSelected(true);
+                selectedPages.append(sender);
+            }
+        } else {
+            if (!sender->isSelected()) {
+                sender->setSelected(true);
+                selectedPages.append(sender);
+            } else {
+                sender->setSelected(false);
+                selectedPages.remove(selectedPages.indexOf(sender));
+            }
         }
 
-        selectedPages.clear();
-
-        if (!sender->isSelected()) {
-            sender->setSelected(true);
-            selectedPages.append(sender);
-        }
 
         // Handle drag
-
         QDrag *drag = new QDrag(this);
         QMimeData *mimeData = new QMimeData;
 
