7962c1d5d76bdff578bbd8052d6025e05fa3b39c
Bui Phuc Duyet <zuyetawarmatik@ubuntu.(none)>
2013-04-17 20:53:41 +0800

    Refactor

--- pdffactory/PDFTableWidget.cpp
+++ pdffactory/PDFTableWidget.cpp
@@ -189,19 +189,19 @@ void PDFTableWidget::moveSelectedPages(QString pathFrom, PDFPageWidget* page){
     moveSelectedPages(pathFrom, page->getName());
 }
 
-void PDFTableWidget::deletePage(PDFPageWidget* page){
-    PDFFileWidget *daddy = (PDFFileWidget*)page->getFather();
+void PDFTableWidget::deletePage(PDFPageWidget* pageWidget){
+    PDFFileWidget *daddy = (PDFFileWidget*)pageWidget->getFather();
     int daddyID = fileWidgets.indexOf(daddy);
-    int pageID = daddy->indexChild(page);
+    int pageID = daddy->indexChild(pageWidget);
 
     pdfJam.removePage(daddyID, daddy->pagesContainerWidget->pageWidgets.size(),pageID);
-    daddy->removeChild(page);
+    daddy->removeChild(pageWidget);
 
-    int spos = selectedPages.indexOf(page);
+    int spos = selectedPages.indexOf(pageWidget);
     if (spos!=-1){
         selectedPages.remove(spos);
     }
-    pageChilds.remove(page->getName());
+    pageChilds.remove(pageWidget->getName());
 
     // PLS ACTIVATE THIS LINE ONCE EVERYTHING HAS BEEN FIXED
     // :D :D :D :D :D :D
@@ -209,18 +209,18 @@ void PDFTableWidget::deletePage(PDFPageWidget* page){
 
 }
 
-void PDFTableWidget::copyPage(PDFPageWidget* page){
+void PDFTableWidget::copyPage(PDFPageWidget* pageWidget){
     copiedPages.clear();
-    copiedPages.push_back(page);
+    copiedPages.push_back(pageWidget);
 
-    PDFFileWidget *daddy = (PDFFileWidget*)page->getFather();
+    PDFFileWidget *daddy = (PDFFileWidget*)pageWidget->getFather();
     int daddyID = fileWidgets.indexOf(daddy);
-    int pageID = daddy->indexChild(page);
+    int pageID = daddy->indexChild(pageWidget);
 
     pdfJam.copyPage(daddyID, pageID,0);
 }
 
-void PDFTableWidget::pastePage(PDFFileWidget* file, int pageID){
+void PDFTableWidget::pastePage(PDFFileWidget* fileWidget, int pageID){
     if (copiedPages.size() > 0){
         PDFPageWidget* page =copiedPages.at(0);
 
@@ -229,10 +229,10 @@ void PDFTableWidget::pastePage(PDFFileWidget* file, int pageID){
         pageWidget->setFather(page->getFather());
         pageWidget->setPopplerPage(page->getPage());
 
-        int fileID = fileWidgets.indexOf(file);
-        pdfJam.pastePage(fileID, file->pagesContainerWidget->pageWidgets.size(), pageID, 0);
+        int fileID = fileWidgets.indexOf(fileWidget);
+        pdfJam.pastePage(fileID, fileWidget->pagesContainerWidget->pageWidgets.size(), pageID, 0);
 
-        file->insertChildAt(pageWidget,pageID);
+        fileWidget->insertChildAt(pageWidget,pageID);
     }
 }
 
