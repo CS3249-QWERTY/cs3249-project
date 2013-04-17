84085a9945b9ad63db3023a72b567d97d1cdacc0
Navie Huynh <quahuynh@gmail.com>
2013-04-18 02:14:22 +0800

    really added Previewgen :P

---
+++ pdffactory/PreviewGen.cpp
@@ -0,0 +1,25 @@
+#include <QtGlobal>
+#include <QtGui>
+#include "PreviewGen.h"
+PreviewGen::PreviewGen()
+{
+
+}
+void PreviewGen::run(){
+        mutex.lock();
+        Poppler::Page *pp =pPage;
+        double instantDpi =dpi;
+        double instantRotation = rotation;
+        mutex.unlock();
+
+        QImage pageImage = pp->renderToImage(instantDpi, instantDpi,-1,-1,-1,-1,rotation);
+        emit updatePreview(pageImage);
+}
+void PreviewGen::render(Poppler::Page* pp,double aDpi,Poppler::Page::Rotation rotation){
+    mutex.lock();
+    pPage= pp;
+    dpi = aDpi;
+    mutex.unlock();
+
+}
+
